7b0e119ae770e0815e20d1bc20fad4d18e04106f
gilad-ben-dor <gilad.bendor@gmail.com>
2023-12-10 08:13:51 +0200

    Gilad changes: some translations never succeed (timeout). Fail after 5 times.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
 
 const MIN_LENGTH_TO_CHECK = 5;
 const MAX_BAD_CHARS_RATIO = 0.2;
+const MAX_REQUEST_RETRIES = 5;
 
 const configuration = new Configuration({
   apiKey: config.OPENAI_API_KEY,
@@ -32,6 +33,7 @@ for (let subtitleFile of subtitles) {
   let previousSubtitles = []
 
   // Loop over all subtitles.
+  subtitlesLoop:
   for (let i = 0; i < subtitle.length; i++) {
     let text = subtitle[i].data.text
     let input = { Input: text }
@@ -41,19 +43,20 @@ for (let subtitleFile of subtitles) {
     let temperature = 0;
 
     // Call GPT for a single subtitle - with retries for non-hebrew translations.
+    let request;
     let result;
-    for (;;) {
+    let firstResult;
+    for (let retry; ; retry++) {
       // Call GPT for a single subtitle - with retries for GPT errors.
-      let completion;
+      let completion = undefined;
       for (;;) {
-        try {
-          completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            temperature,
-            messages: [
-              {
-                role: "system",
-                content: `
+        request = {
+          model: "gpt-3.5-turbo",
+          temperature,
+          messages: [
+            {
+              role: "system",
+              content: `
                     You are a program responsible for translating subtitles.
                     Your task is to output the specified target language based on the input text.
                     Please do not create the following subtitles on your own.
@@ -65,18 +68,29 @@ for (let subtitleFile of subtitles) {
                     Target language: ${config.TARGET_LANGUAGE}.
                     Never output anything other that is not ${config.TARGET_LANGUAGE}.
                   `.replace(/\s+/g, ' ').trim()
-              },
-              ...previousSubtitles.slice(-4),
-              {
-                role: "user",
-                content: JSON.stringify(input)
-              }
-            ],
-          }, {timeout: 60 * 1000});
+            },
+            ...previousSubtitles.slice(-4),
+            {
+              role: "user",
+              content: JSON.stringify(input)
+            }
+          ],
+        };
+        try {
+          completion = await openai.createChatCompletion(request, {timeout: 60 * 1000});
           break;
         } catch (e) {
-          console.error(`Error:    ${e}`);
-          console.log('retrying...'.red);
+          console.error('Request: '.red, request);
+          console.error('Error: '.red, e);
+
+          if (retry < MAX_REQUEST_RETRIES) {
+            console.log('Retrying...'.red);
+          } else {
+            console.error(`Giving up after ${MAX_REQUEST_RETRIES} retries`.red);
+            subtitle[i].data.text = `(בלתי ניתן לתרגום)\n(can't be translated)`
+
+            continue subtitlesLoop;
+          }
         }
       }
 
@@ -113,15 +127,27 @@ for (let subtitleFile of subtitles) {
         break;
       } else {
         // Translation is bad (English).
-        console.error(`Bad chars ratio: ${badCharsRatio}. Retrying...\nTranslation: ${result.replace(/\n/g, '\n             ')}\n`.red);
+        if (!firstResult) {
+            firstResult = result;
+        }
+        if (temperature >= 1.5) {
+            // Give up - use the translation of the first temperature.
+            result = firstResult;
+            console.error('Request: '.red, request);
+            console.error('Response: '.red, result);
+            console.error(`Bad chars ratio: ${badCharsRatio}. Giving up - using translation of low temperature`.red);
+            break;
+        }
         temperature += 0.1;
+        console.error('Request: '.red, request);
+        console.error('Response: '.red, result);
+        console.error(`Bad chars ratio: ${badCharsRatio}. Retrying with temperature ${temperature}...\nTranslation: ${result.replace(/\n/g, '\n             ')}\n`.red);
       }
     }
 
     // One subtitle is successfully translated.
     previousSubtitles.push({ role: "user", content: JSON.stringify(input) })
     previousSubtitles.push({ role: 'assistant', content: JSON.stringify({ ...input, Input: result }) })
-    // console.log(`${subtitle[i].data.text}`.blue)
     subtitle[i].data.text = `${result}\n${text}`
 
     console.log(`-----------------`.gray)
