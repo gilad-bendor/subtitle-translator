4e4c5ef1343ccaee973470b6ab9489829c945a6d
gilad-ben-dor <gilad.bendor@gmail.com>
2024-03-10 08:24:43 +0200

    Gilad changes: recursing ./src

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import colors from 'colors'
+import 'colors'
 import { Configuration, OpenAIApi } from 'openai'
 import { parseSync, stringifySync } from 'subtitle'
 const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
@@ -14,30 +14,45 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-let subtitles = fs.readdirSync('./src')
 let supportExtensions = ['srt', 'vtt']
-for (let subtitleFile of subtitles) {
-  if (!supportExtensions.includes(subtitleFile.split('.').pop())) {
-    console.log(`Skipping non-subtitle ${subtitleFile}`.gray);
-    continue
+
+await handleDir('./src', './res');
+
+async function handleDir(sourceDir, targetDir) {
+  let subtitles = fs.readdirSync(sourceDir)
+  for (let subtitleFile of subtitles) {
+    const stat = fs.statSync(`${sourceDir}/${subtitleFile}`, {throwIfNoEntry: true});
+    if (stat.isDirectory()) {
+      await handleDir(`${sourceDir}/${subtitleFile}`, `${targetDir}/${subtitleFile}`);
+    } else if (stat.isFile()) {
+      await handleFile(`${sourceDir}/${subtitleFile}`, `${targetDir}/${subtitleFile}`);
+    }
   }
-  if (fs.statSync(`./res/${subtitleFile}`, { throwIfNoEntry: false })) {
+}
+
+async function handleFile(subtitleFile, targetFile) {
+  const suffix = subtitleFile.split('.').pop();
+  if (!supportExtensions.includes(suffix)) {
+    console.log(`Skipping non-subtitle ${subtitleFile}`.gray);
+    return;
+  }
+  if (fs.statSync(targetFile, {throwIfNoEntry: false})) {
     console.log(`Skipping already translated ${subtitleFile}`.gray);
-    continue;
+    return;
   }
 
   console.log(`\n\n==============================     ${subtitleFile}     ==============================\n`);
-  let subtitle = fs.readFileSync(`./src/${subtitleFile}`, 'utf8')
-  subtitle = parseSync(subtitle)
-  subtitle = subtitle.filter(line => line.type === 'cue')
+  let subtitle = fs.readFileSync(subtitleFile, 'utf8');
+  subtitle = parseSync(subtitle);
+  subtitle = subtitle.filter(line => line.type === 'cue');
 
-  let previousSubtitles = []
+  let previousSubtitles = [];
 
   // Loop over all subtitles.
   subtitlesLoop:
   for (let i = 0; i < subtitle.length; i++) {
     let text = subtitle[i].data.text
-    let input = { Input: text }
+    let input = {Input: text}
     if (subtitle[i + 1]) {
       input.Next = subtitle[i + 1].data.text
     }
@@ -47,10 +62,10 @@ for (let subtitleFile of subtitles) {
     let request;
     let result;
     let firstResult;
-    for (let retry; ; retry++) {
+    for (let retry = 0; ; retry++) {
       // Call GPT for a single subtitle - with retries for GPT errors.
       let completion = undefined;
-      for (;;) {
+      for (; ;) {
         request = {
           model: "gpt-3.5-turbo",
           temperature,
@@ -58,17 +73,17 @@ for (let subtitleFile of subtitles) {
             {
               role: "system",
               content: `
-                    You are a program responsible for translating subtitles.
-                    Your task is to output the specified target language based on the input text.
-                    Please do not create the following subtitles on your own.
-                    Please do not output any text other than the translation.
-                    You will receive the subtitles as array that needs to be translated,
-                     as well as the previous translation results and next subtitle.
-                    If you need to merge the subtitles with the following line, simply repeat the translation.
-                    Please transliterate the person's name into the local language.
-                    Target language: ${config.TARGET_LANGUAGE}.
-                    Never output anything other that is not ${config.TARGET_LANGUAGE}.
-                  `.replace(/\s+/g, ' ').trim()
+                  You are a program responsible for translating subtitles.
+                  Your task is to output the specified target language based on the input text.
+                  Please do not create the following subtitles on your own.
+                  Please do not output any text other than the translation.
+                  You will receive the subtitles as array that needs to be translated,
+                   as well as the previous translation results and next subtitle.
+                  If you need to merge the subtitles with the following line, simply repeat the translation.
+                  Please transliterate the person's name into the local language.
+                  Target language: ${config.TARGET_LANGUAGE}.
+                  Never output anything other that is not ${config.TARGET_LANGUAGE}.
+                `.replace(/\s+/g, ' ').trim()
             },
             ...previousSubtitles.slice(-4),
             {
@@ -113,7 +128,7 @@ for (let subtitleFile of subtitles) {
             } else {
               // Strange, response is not in the right format.
               console.log('###'.red)
-              console.log(e.toString().red)
+              console.log(e.stack.red)
               console.log(result?.red)
               console.log('###'.red)
             }
@@ -142,15 +157,15 @@ for (let subtitleFile of subtitles) {
       } else {
         // Translation is bad (English).
         if (!firstResult) {
-            firstResult = result;
+          firstResult = result;
         }
         if (temperature >= 1.5) {
-            // Give up - use the translation of the first temperature.
-            result = firstResult;
-            console.error('Request: '.red, request);
-            console.error('Response: '.red, result);
-            console.error(`Bad chars ratio: ${badCharsRatio}. Giving up - using translation of low temperature`.red);
-            break;
+          // Give up - use the translation of the first temperature.
+          result = firstResult;
+          console.error('Request: '.red, request);
+          console.error('Response: '.red, result);
+          console.error(`Bad chars ratio: ${badCharsRatio}. Giving up - using translation of low temperature`.red);
+          break;
         }
         temperature += 0.1;
         console.error('Request: '.red, request);
@@ -160,8 +175,8 @@ for (let subtitleFile of subtitles) {
     }
 
     // One subtitle is successfully translated.
-    previousSubtitles.push({ role: "user", content: JSON.stringify(input) })
-    previousSubtitles.push({ role: 'assistant', content: JSON.stringify({ ...input, Input: result }) })
+    previousSubtitles.push({role: "user", content: JSON.stringify(input)})
+    previousSubtitles.push({role: 'assistant', content: JSON.stringify({...input, Input: result})})
     subtitle[i].data.text = `${result}\n${text}`
 
     console.log(`-----------------`.gray)
@@ -169,6 +184,5 @@ for (let subtitleFile of subtitles) {
     console.log(`${result}`.green)
     console.log(`${text}`.white)
   }
-  fs.writeFileSync(`./res/${subtitleFile}`, stringifySync(subtitle, { format: 'srt' }))
-
+  fs.writeFileSync(targetFile, stringifySync(subtitle, {format: 'srt'}));
 }
